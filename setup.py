8791aba89759d12aac7110798c34e06ec95bae5e
Henry Marment <henry@adsquare.com>
2018-05-28 16:33:54 +0200

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="py-insightly",
-    version="0.0.1",
+    version="0.0.2",
 
     description='Python wrapper around the Insightly API',
     long_description=open('README.md').read(),
